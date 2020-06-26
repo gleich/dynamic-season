d9a5002ac0cd1fff6a4f45b1e3398f6da697811c
Matthew Gleich <matthewgleich@gmail.com>
2020-06-26 18:28:25 -0400

    ✨ Finalize latitude
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => this.setState({ lat: position.coords.latitude }),
       (err) => console.log(err)
     );
-    return <div>Latitude: </div>;
+  }
+
+  render() {
+    return <div>Latitude: {this.state.lat} </div>;
   }
 }
 
