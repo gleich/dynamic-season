882baa6c750f22dae0dc2266315e4354753315bf
Matthew Gleich <matthewgleich@gmail.com>
2020-06-27 00:23:58 -0400

    ✨ Add longitude
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,15 +5,35 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    this.state = { lat: null, long: null, errorMessage: '' };
     window.navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({ lat: position.coords.latitude }),
-      (err) => console.log(err)
+      (position) =>
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
   render() {
-    return <div>Latitude: {this.state.lat} </div>;
+    return (
+      <div>
+        Latitude:{' '}
+        {this.state.errorMessage === ''
+          ? this.state.lat == null
+            ? '🔄 Loading'
+            : this.state.lat
+          : this.state.errorMessage}
+        <br />
+        Longitude:{' '}
+        {this.state.errorMessage === ''
+          ? this.state.lat == null
+            ? '🔄 Loading'
+            : this.state.long
+          : this.state.errorMessage}
+      </div>
+    );
   }
 }
 
