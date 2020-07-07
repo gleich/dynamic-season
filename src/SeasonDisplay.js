4a4d87443cce14d37225c57be132d81bda2b0bdc
Matthew Gleich <matthewgleich@gmail.com>
2020-07-06 23:02:16 -0400

    💄 Add styling
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
 const seasonConfig = {
@@ -22,10 +23,10 @@ const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   const { text, iconName } = seasonConfig[season];
   return (
-    <div>
-      <i className={`${iconName} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left ${iconName} icon massive`} />
       <h1>{text}</h1>
-      <i className={`${iconName} icon`} />
+      <i className={`icon-right ${iconName} icon massive`} />
     </div>
   );
 };
